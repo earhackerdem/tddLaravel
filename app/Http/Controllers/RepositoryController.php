2c7406f6534d52694f70a6681736079820971a4d
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-07-05 22:06:07 -0500

    20 - Validación de datos

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RepositoryRequest;
 use App\Models\Repository;
 use Illuminate\Http\Request;
 
@@ -28,12 +29,8 @@ class RepositoryController extends Controller
         return view('repositories.create');
     }
 
-    public function store(Request $request)
+    public function store(RepositoryRequest $request)
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required'
-        ]);
 
         $request->user()->repositories()->create($request->all());
 
@@ -49,12 +46,8 @@ class RepositoryController extends Controller
         return view('repositories.edit',compact('repository'));
     }
 
-    public function update(Request $request,Repository $repository)
+    public function update(RepositoryRequest $request,Repository $repository)
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required'
-        ]);
 
         if($request->user()->id != $repository->user_id){
             abort(403);
